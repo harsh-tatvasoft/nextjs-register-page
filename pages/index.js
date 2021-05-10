3eadb21edf80642a3505005c68f70a6a778730e9
Harsh Patel <harsh.tatvasoft@gmail.com>
2021-05-10 18:11:25 +0530

    Added Fomik validation

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
-import { useFormik } from "formik";
+import { Formik, Form, Field } from "formik";
 import Head from "next/head";
 
-export default function Home() {
-    const formik = useFormik({
-        initialValues: {
-            email: "",
-            password: "",
-        },
-        onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 10));
-        },
-    });
+function validateEmail(value) {
+    let error;
+    if (!value) {
+        error = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+        error = "Invalid email address";
+    }
+    return error;
+}
 
+function validateUsername(value) {
+    let error;
+    if (!value) {
+        error = "Required";
+    } else if (value === "admin") {
+        error = "Nice try!";
+    }
+    return error;
+}
+
+export default function Home() {
     return (
         <div>
             <Head>
@@ -19,29 +29,37 @@ export default function Home() {
             </Head>
 
             <div>
-                <form onSubmit={formik.handleSubmit}>
-                    <label htmlFor="email">Email Address</label>{" "}
-                    <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                    />
-                    <br />
-                    <br />
-                    <label htmlFor="password">Password</label>{" "}
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
-                    />
-                    <br />
-                    <br />
-                    <button type="submit">Submit</button>
-                </form>
+                <Formik
+                    initialValues={{
+                        username: "",
+                        email: "",
+                    }}
+                    onSubmit={(values) => {
+                        console.log(values);
+                        alert("Successfully submitted!!");
+                    }}
+                >
+                    {({ errors, touched }) => (
+                        <Form>
+                            <label htmlFor="email">Email Address</label>{" "}
+                            <Field name="email" validate={validateEmail} />
+                            {errors.email && touched.email && (
+                                <div>{errors.email}</div>
+                            )}
+                            <br />
+                            <label htmlFor="username">Username</label>{" "}
+                            <Field
+                                name="username"
+                                validate={validateUsername}
+                            />
+                            <br />
+                            {errors.username && touched.username && (
+                                <div>{errors.username}</div>
+                            )}
+                            <button type="submit">Submit</button>
+                        </Form>
+                    )}
+                </Formik>
             </div>
         </div>
     );
